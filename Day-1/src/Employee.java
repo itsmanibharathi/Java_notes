7603c44e6afb195113bc6ab85c92c0201cd0166f
manibharathi <manibharatidct@gmail.com>
2023-06-13 18:39:45 +0530

    init

---
+++ Day-1/src/Employee.java
@@ -0,0 +1,41 @@
+public class Employee {
+    int id;
+    String name;
+    double salary;
+    static String comp= "   keg";
+    Employee(){
+        this.id=id;
+        this.name=name;
+        this.salary = salary;
+    }
+    Employee(int id ,String name,double salary){
+        this.id=id;
+        this.name=name;
+        this.salary = salary;
+    }
+    void setValue(int id ,String name,double salary){
+        this.id=id;
+        this.name=name;
+        this.salary= salary;
+    }
+    void displaye() {
+        System.out.println(this.id + " " + this.name + " " + this.salary+ this.comp);
+    }
+    static void change(){
+        comp = "kec";
+    }
+    public static void main(String[] args) {
+        Employee obj = new Employee();
+        obj.setValue(1,"Mani",1000);
+        obj.displaye();
+        Employee obj1 = new Employee(2,"kali",123);
+        obj1.displaye();
+        Employee.change();
+        obj.displaye();
+        obj1.displaye();
+        System.out.println(); //sout
+
+
+    }
+}
+
---
+++ Day-1/src/Main.java
@@ -0,0 +1,18 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        // Press Alt+Enter with your caret at the highlighted text to see how
+        // IntelliJ IDEA suggests fixing it.
+        Employee obj = new Employee();
+        obj.setValue(1,"Mani",1000);
+        obj.displaye();
+        Employee obj1 = new Employee(2,"kali",123);
+        obj1.displaye();
+        Employee.change();
+        obj.displaye();
+        obj1.displaye();
+        System.out.println(); //sout
+
+    }
+}
