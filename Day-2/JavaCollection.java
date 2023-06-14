ee04c8dee5db3b0f8128d821174af2abf85dcbbe
manibharathi <manibharatidct@gmail.com>
2023-06-14 12:31:54 +0530

    set Collection

--- Day-2/JavaCollection.java
+++ Day-2/JavaCollection.java
@@ -1,7 +1,7 @@
 import java.util.*;
 public class JavaCollection{
     public static void main(String arr[]){
-        int arrs[]={1,2,3,4,5,1,2,3,4,5};
+        int arrs[]={5,4,3,2,1};
         // List<Integer> al= new ArrayList<>();
         // for(int i=0;i<arrs.length;i++)
         //     al.add(arrs[i]);
@@ -9,13 +9,24 @@ public class JavaCollection{
         // for(Integer x:al)
         //     System.out.print(x);
         Set <Integer> sl = new HashSet<>();
+        Set <Integer> tsl = new TreeSet<>();
+        Set <Integer> lsl = new LinkedHashSet<>();
         for(int i=0;i<arrs.length;i++){
             sl.add(arrs[i]);
+            tsl.add(arrs[i]);
+            lsl.add(arrs[i]);
         }
         // for(int i=0;i<sl.size;i++){
         //     System.out.print(sl.get(i));
         // }
+    
         for(Integer x:sl)
-            System.out.print(x+"   ");
+            System.out.print(x); // 1 2 3 4 5
+        System.out.println("");
+        for(Integer x:tsl)
+            System.out.print(x); // 1 2 3 4 5
+        System.out.println("");
+        for(Integer x:lsl)
+            System.out.print(x); // 5 4 3 2 1
     }
 }
